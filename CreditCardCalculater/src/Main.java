9c306cde5530e30b03d351e66f068557948bb4d9
jungwoo-0530 <am7227@naver.com>
2022-02-15 03:57:17 +0900

    Feat: "신한은행 추가"

--- CreditCardCalculater/src/Main.java
+++ CreditCardCalculater/src/Main.java
@@ -12,16 +12,28 @@ public class Main {
 
   public static void main(String[] args) throws IOException, ParseException {
 
-    String path = "/Users/jungwoo/Desktop/git/CalculaterBill/CreditCardCalculater/hundai.csv";
+    String path = "/Users/jungwoo/Desktop/git/CalculaterBill/CreditCardCalculater/hyundai.csv";
 
-    Csv csv = new Csv();
-    Calculater calculater = new Calculater(csv.loadCsv(path));
+    Csv hyundaiCsv = new Csv();
+    Calculater hyundaiCalculater = new Calculater(hyundaiCsv.loadCsv(path));
 
     Scanner sc = new Scanner(System.in);
     System.out.println("지금으로부터 몇달까지 계산할까요?");
     int targetMonth = sc.nextInt();
 
-    calculater.resultPrint(targetMonth);
+    System.out.println("현대카드");
+    hyundaiCalculater.resultPrint(targetMonth);
+
+    System.out.println("------------------------------------------------------------------------");
+    System.out.println("신한카드");
+
+    path = "/Users/jungwoo/Desktop/git/CalculaterBill/CreditCardCalculater/Sinhan.csv";
+    Csv sinhanCsv = new Csv();
+    Calculater sinhanCalculater = new Calculater(sinhanCsv.loadCsv(path));
+
+    sinhanCalculater.resultPrint(targetMonth);
+
+
 
 
 
